fcd2143646625fe15d5c8e1158456862c2c24374
Michal Dostal <michal.dostal1@tul.cz>
2022-10-13 08:40:46 +0200

    Edit pracovni soubor

--- cviceni_03/src/cviceni_03/Cviceni_03.java
+++ cviceni_03/src/cviceni_03/Cviceni_03.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package cviceni_03;
 
 /**
  *
- * @author michal
+ * @author ZADEJTE_SVE_JMENO
  */
 public class Cviceni_03 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
