5fc8cc7d03c39d86bb9ea1aa6661a75876901c97
Michal Dostal <michal.dostal1@tul.cz>
2022-10-13 11:22:51 +0200

    Add cviceni_04

---
+++ cviceni_04/src/cviceni_04/Cviceni_04.java
@@ -0,0 +1,21 @@
+
+package cviceni_04;
+
+/**
+ *
+ * @author ZADEJTE_SVE_JMENO
+ */
+public class Cviceni_04 {
+    
+    /*
+    P L Á N  C V I Č E N Í
+    - statické metody a proměnné
+    - ASCII art v konzolovém výstupu
+    - opakování tvorby menu
+    */
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
