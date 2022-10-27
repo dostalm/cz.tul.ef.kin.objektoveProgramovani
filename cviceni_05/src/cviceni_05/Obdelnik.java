2e0d2b63a99a77dcf4a9bda66a487ad4ec07eafd
Michal Dostál <michal.dostal1@tul.cz>
2022-10-27 07:19:01 +0200

    Add cviceni_05

---
+++ cviceni_05/src/cviceni_05/Obdelnik.java
@@ -0,0 +1,9 @@
+package cviceni_05;
+
+/**
+ *
+ * @author ZADEJTE_SVE_JMENO
+ */
+public class Obdelnik {
+    
+}
---
+++ cviceni_05/src/cviceni_05/Pole.java
@@ -0,0 +1,18 @@
+package cviceni_05;
+
+/**
+ *
+ * @author ZADEJTE_SVE_JMENO
+ */
+public class Pole {
+
+    /**
+     * Cílem dnešního cvičení je si procvičit práci s polemi a objekty.
+     * Úkoly:
+     *  - 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
